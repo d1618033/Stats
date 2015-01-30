fa0a50d9f0b36b03ae60224f145a4d7732451d2f
d1618033 <d1618033@gmail.com>
2015-01-30 12:31:37 +0200

    using Icollection instead of Ienumerable

--- BasicStats.cs
+++ BasicStats.cs
@@ -6,11 +6,11 @@ namespace Stats
 {
 	public static class BasicStats
 	{
-		public static double mean(IEnumerable<double> array)
+		public static double mean(ICollection<double> array)
 		{
 			return array.Sum() / array.Count();
 		}
-		public static double variance(IEnumerable<double> array)
+		public static double variance(ICollection<double> array)
 		{
 			if (!array.Any())
 			{
@@ -19,11 +19,11 @@ namespace Stats
 			double m = mean (array);
 			return array.Sum (a => (a-m) * (a-m)) / (array.Count() - 1);
 		}
-		public static double std(IEnumerable<double> array)
+		public static double std(ICollection<double> array)
 		{
 			return Math.Sqrt (variance (array));
 		}
-		public static double cov(IEnumerable<double> array1, IEnumerable<double> array2)
+		public static double cov(ICollection<double> array1, ICollection<double> array2)
 		{
 			if (array1.Count() != array2.Count())
 			{
