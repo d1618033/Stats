c7258b9b0e33ad97e233f7361a74f4d9d8b75217
d1618033 <d1618033@gmail.com>
2015-01-31 22:39:07 +0200

    added mock random

---
+++ MockRandom.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stats
+{
+	public class MockRandom: Random
+	{
+		private IEnumerator<int> seeds;
+
+		public MockRandom () : base()
+		{
+			IList<int> temp = new int[] { 0 };
+			seeds = temp.GetEnumerator();
+		}
+
+		public IEnumerable<int> Seeds {
+			set{
+				IEnumerator<int> temp = value.GetEnumerator ();
+				if (!temp.MoveNext())
+					throw new ArgumentException ("Seeds cannot be empty");
+				temp.Reset();
+				seeds = temp;
+			}
+		}
+		public override int Next(int minValue, int maxValue)
+		{
+			return clamp(getNextSeed(), minValue, maxValue-1);
+		}
+		public override int Next()
+		{
+			return getNextSeed ();
+		}
+		public override int Next (int maxValue)
+		{
+			return clamp (getNextSeed (), maxValue-1);
+		}
+		private int clamp(int x, int min, int max)
+		{
+			return x < min ? min : x > max ? max : x;
+		}
+		private int clamp(int x, int max)
+		{
+			return x > max ? max : x;
+		}
+		private int getNextSeed()
+		{
+			if (!seeds.MoveNext ()) {
+				seeds.Reset ();
+				seeds.MoveNext ();
+			}
+			return seeds.Current;
+		}
+	}
+}
+
+
---
+++ TestMockRandom.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Stats
+{
+	[TestFixture]
+	public class TestMockRandom
+	{
+		private MockRandom mr;
+		[SetUp]
+		public void setUp ()
+		{
+			mr = new MockRandom ();
+		}
+
+		[Test]
+		public void TestDefaultIs0()
+		{
+			for (int i = 0; i < 10; i++)
+				Assert.AreEqual (0, mr.Next ());
+		}
+		[Test]
+		public void TestSetSeeds()
+		{
+			mr.Seeds = new int[] { 1, 2 };
+			Assert.AreEqual (1, mr.Next ());
+			Assert.AreEqual (2, mr.Next ());
+		}
+		[Test]
+		public void TestNextMax()
+		{
+			mr.Seeds = new int[] { 1, 5 };
+			Assert.AreEqual (1, mr.Next (3));
+			Assert.AreEqual (3, mr.Next (4));
+		}
+		[Test]
+		public void TestNextMinMax()
+		{
+			mr.Seeds = new int[] { 1, 5, 7, 11};
+			Assert.AreEqual (1, mr.Next (1, 10));
+			Assert.AreEqual (4, mr.Next (4, 5), 4);
+			Assert.AreEqual (3, mr.Next (2, 4));
+			Assert.AreEqual (20, mr.Next (20, 23));
+		}
+		[Test] 
+		public void TestCycles()
+		{
+			mr.Seeds = new int[] { 3, 4, 5 };
+			for (int i = 0; i < 10; i++) 
+			{
+				Assert.AreEqual (3, mr.Next ());
+				Assert.AreEqual (4, mr.Next ());
+				Assert.AreEqual (5, mr.Next ());
+			}
+		}
+
+	}
+}
+
