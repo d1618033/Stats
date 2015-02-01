885d3fa330e4f6b975f68c2d631ff380e745e130
d1618033 <d1618033@gmail.com>
2015-02-01 20:55:27 +0200

    added root finder

---
+++ RootFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Stats
+{
+	public static class RootFinder
+	{
+		public static double solve(Func<double, double> f, double a, double b)
+		{
+			if (f (a) * f (b) > 0)
+				throw new ArgumentException ("f(a) and f(b) must have opposite signs");
+			return 0;
+		}
+	}
+}
+
---
+++ TestRootFinder.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Stats
+{
+	[TestFixture]
+	public class TestRootFinder
+	{
+		[Test]
+		public void TestThrowsErrorIfFunctionAtArgumentsHaveSameSign ()
+		{
+			Assert.Throws (typeof(ArgumentException), () => RootFinder.solve (x => x * x - 1, -10, -5));
+			Assert.Throws (typeof(ArgumentException), () => RootFinder.solve (x => x * x - 1, 10, 5));
+			Assert.Throws (typeof(ArgumentException), () => RootFinder.solve (x => x * x - 1, -0.5, 0.5));
+		}
+	}
+}
+
+
