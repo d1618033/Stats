515488d0ab339c4e98018a80608e817a2cf9b765
d1618033 <d1618033@gmail.com>
2015-02-01 22:05:08 +0200

    switched alpha to confidence, added tests for out of bounds confidence

--- ConfidenceInterval.cs
+++ ConfidenceInterval.cs
@@ -10,33 +10,23 @@ namespace Stats
 	}
 	public class NormalConfidenceInterval: IConfidenceInterval
 	{
-		public double Alpha {
-			set {
-				if (value < 0 || value > 1)
-					throw new ArgumentException ("Alpha must be between 0 and 1");
-				alpha = value;
-			}
-		}
-
-		#region IConfidenceInterval implementation
-
-		double IConfidenceInterval.Low ()
+		public double Low ()
 		{
 			return nrv.icdf (alpha/2);
 		}
 
-		double IConfidenceInterval.High ()
+		public double High ()
 		{
 			return nrv.icdf (1 - alpha/2);
 		}
 
-		#endregion
-
-		private double alpha;
-		private NormalRandomVariable nrv;
-		public NormalConfidenceInterval (double mean, double std, double alpha)
+		private readonly double alpha;
+		private readonly NormalRandomVariable nrv;
+		public NormalConfidenceInterval (double mean, double std, double confidence)
 		{
-			this.Alpha = alpha;
+			if (confidence < 0 || confidence > 1)
+				throw new ArgumentException ("Confidence must be between 0 and 1");
+			alpha = 1 - confidence;
 			nrv = new NormalRandomVariable (mean, std);
 		}
 	}
---
+++ TestConfidenceInterval.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Stats
+{
+	[TestFixture]
+	public class TestNormalConfidenceInterval
+	{
+		[Test]
+		public void TestStandard95 ()
+		{
+			NormalConfidenceInterval nci = new NormalConfidenceInterval(0, 1, 0.95);
+			Assert.AreEqual (1.96, nci.High (), 0.01);
+			Assert.AreEqual (-1.96, nci.Low (), 0.01);
+		}
+		[Test]
+		public void TestThrowsErrorIfConfidenceIsGreaterThan1()
+		{
+			Assert.Throws(typeof(ArgumentException), () => new NormalConfidenceInterval(0, 1, 1.1));
+		}
+		[Test]
+		public void TestThrowsErrorIfConfidenceIsLessThan0()
+		{
+			Assert.Throws(typeof(ArgumentException), () => new NormalConfidenceInterval(0, 1, -0.1));
+		}
+	}
+}
+
