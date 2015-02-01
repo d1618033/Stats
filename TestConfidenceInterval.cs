cfcc3cbe89ec9e2b54977463fc77a55a9801c190
d1618033 <d1618033@gmail.com>
2015-02-01 22:18:32 +0200

    added test for confidence interval for non standard

--- TestConfidenceInterval.cs
+++ TestConfidenceInterval.cs
@@ -14,6 +14,13 @@ namespace Stats
 			Assert.AreEqual (-1.96, nci.Low (), 0.01);
 		}
 		[Test]
+		public void TestNonStandard95 ()
+		{
+			NormalConfidenceInterval nci = new NormalConfidenceInterval(1, 4, 0.95);
+			Assert.AreEqual (8.84, nci.High (), 0.01);
+			Assert.AreEqual (-6.84, nci.Low (), 0.01);
+		}
+		[Test]
 		public void TestThrowsErrorIfConfidenceIsGreaterThan1()
 		{
 			Assert.Throws(typeof(ArgumentException), () => new NormalConfidenceInterval(0, 1, 1.1));
