a3c99150f12de0bd15065284723bb2a6376bfec3
d1618033 <d1618033@gmail.com>
2015-02-03 21:47:55 +0200

    added linear interpolation

---
+++ LinearInterpolation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stats
+{
+	public class LinearInterpolation: ICurveFit
+	{
+		private IList<double> x;
+		private IList<double> y;
+		private bool fitWasRun;
+
+		public LinearInterpolation ()
+		{
+			fitWasRun = false;
+		}
+
+		#region ICurveFit implementation
+
+		public void fit (IList<double> x, IList<double> y)
+		{
+			if (x.Count != y.Count)
+				throw new ArgumentException ("X and Y must be of the same size");
+			if (x.Count <= 1)
+				throw new ArgumentException ("X and Y must not be empty");
+			this.x = x;
+			this.y = y;
+			fitWasRun = true;
+		}
+
+		public double predict (double x0)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+	}
+}
+
---
+++ TestLinearInterpolation.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Stats
+{
+	[TestFixture ()]
+	public class TestLinearInterpolation
+	{
+		private LinearInterpolation li;
+		[SetUp]
+		public void setUp()
+		{
+			li = new LinearInterpolation ();
+		}
+		[Test]
+		public void TestFitNotSameSizeThrowsError ()
+		{
+			double[] x = { 1, 5, 7 };
+			double[] y = { 2, 3 };
+			Assert.Throws (typeof(ArgumentException), () => li.fit (x, y));
+		}
+		[Test]
+		public void TestFitOfSize0ThrowsError ()
+		{
+			double[] x = {};
+			double[] y = {};
+			Assert.Throws (typeof(ArgumentException), () => li.fit (x, y));
+		}
+		[Test]
+		public void TestFitOfSize1ReturnsSameY ()
+		{
+			double[] x = {1};
+			double[] y = {10};
+			li.fit (x, y);
+			Assert.AreEqual (10, li.predict (1));
+		}
+		[Test]
+		public void TestFitOfSize1ThrowsErrorForOutOfBounds ()
+		{
+			double[] x = {1};
+			double[] y = {10};
+			li.fit (x, y);
+			Assert.Throws(typeof(ArgumentException), () => li.predict (2));
+			Assert.Throws(typeof(ArgumentException), () => li.predict (0));
+		}
+	}
+}
+
