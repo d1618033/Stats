4b990d9306e0f94c40a192d1ec0953e994743c14
d1618033 <d1618033@gmail.com>
2015-02-06 19:28:35 +0200

    fixed up poly interp

--- PolynomialInterpolation.cs
+++ PolynomialInterpolation.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Stats
 {
-	public class PolynomialInterpolation
+	public class PolynomialInterpolation: ICurveFit
 	{
+		private IList<double> x;
+		private IList<double> y;
+		private bool fitWasRun;
+
 		public PolynomialInterpolation ()
 		{
+			fitWasRun = false;
 		}
+
+		#region ICurveFit implementation
+		private class Point{
+			public double x{ get; set; }
+			public double y{ get; set; }
+			public Point(double x, double y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+		public void fit (IList<double> x, IList<double> y)
+		{
+			if (x.Count != y.Count)
+				throw new ArgumentException ("X and Y must be of the same size");
+			if (x.Count == 0)
+				throw new ArgumentException ("X and Y must not be empty");
+			fixDuplicates (x, y);
+			fitWasRun = true;
+		}
+		private void fixDuplicates(IList<double> x, IList<double> y)
+		{
+			IList<Point> data = x.Select ((xi, i) => new Point(xi,y [i])).OrderBy (e => e.x).ToList ();
+			IList<Point> newData = new List<Point> ();
+			newData.Add (data.First ());
+			for (int i = 1; i < data.Count; i++) {
+				if (data [i].x == data [i - 1].x) {
+					if (data [i].y != data [i - 1].y)
+						throw new ArgumentException ("Same X value must have same Y value");
+				} else {
+					newData.Add (data[i]);
+				}
+			}
+			this.x = newData.Select (p => p.x).ToList();
+			this.y = newData.Select (p => p.y).ToList();
+
+		}
+
+		public double predict (double x0)
+		{
+			if (!fitWasRun)
+				throw new ArgumentException ("Fit must be run before predict");
+			if (this.x.Count == 1) {
+				if (x0 > this.x[0] || x0 < this.x[0])
+					throw new ArgumentException ("x0 is out of bounds");
+				return this.y [0];
+			}
+			int index = Array.BinarySearch (x.ToArray(), x0);
+			if (index > 0)
+				return y [index];
+			else {
+				double y0 = 0;
+				for (int i = 0; i < x.Count; i++) {
+					double p = 1;
+					for (int j = 0; j < x.Count; j++) {
+						if (j == i)
+							continue;
+						p *= (x0 - x [j]) / (x [i] - x [j]);
+					}
+					p *= y [i];
+					y0 += p;
+				}
+				return y0;
+			}
+		}
+
+		#endregion
 	}
 }
-
--- TestPolynomialInterpolation.cs
+++ TestPolynomialInterpolation.cs
@@ -6,9 +6,94 @@ namespace Stats
 	[TestFixture]
 	public class TestPolynomialInterpolation
 	{
-		[Test]
-		public void TestCase ()
+		PolynomialInterpolation pi;
+		[SetUp]
+		public void setUp()
 		{
+			pi = new PolynomialInterpolation ();
+		}
+		[Test]
+		public void TestFitNotSameSizeThrowsError ()
+		{
+			double[] x = { 1, 5, 7 };
+			double[] y = { 2, 3 };
+			Assert.Throws (typeof(ArgumentException), () => pi.fit (x, y));
+		}
+		[Test]
+		public void TestFitOfSize0ThrowsError ()
+		{
+			double[] x = {};
+			double[] y = {};
+			Assert.Throws (typeof(ArgumentException), () => pi.fit (x, y));
+		}
+		[Test]
+		public void TestFitOfSize1ReturnsSameY ()
+		{
+			double[] x = {1};
+			double[] y = {10};
+			pi.fit (x, y);
+			Assert.AreEqual (10, pi.predict (1));
+		}
+		[Test]
+		public void TestFitOfSize1NotEqualToXThrowsErrorForOutOfBounds ()
+		{
+			double[] x = {1};
+			double[] y = {10};
+			pi.fit (x, y);
+			Assert.Throws(typeof(ArgumentException), () => pi.predict (2));
+			Assert.Throws(typeof(ArgumentException), () => pi.predict (0));
+		}
+		[Test]
+		public void TestFitOfSize2ForXsReturnsSameY ()
+		{
+			double[] x = {5, 6};
+			double[] y = {7, 100};
+			pi.fit (x, y);
+			for (int i=0; i<x.Length; i++)
+				Assert.AreEqual (y [i], pi.predict (x [i]), 1e-10);
+
+		}
+		[Test]
+		public void TestFitOfSize2InTheMiddle ()
+		{
+			double[] x = {5, 6};
+			double[] y = {7, 17};
+			pi.fit (x, y);
+			Assert.AreEqual (12, pi.predict (5.5), 1e-10);
+
+		}
+		[Test]
+		public void TestFitOfSize2ThreeQuarters ()
+		{
+			double[] x = {5, 9};
+			double[] y = {7, 19};
+			pi.fit (x, y);
+			Assert.AreEqual (16, pi.predict (8), 1e-10);
+
+		}
+		[Test]
+		public void TestSameXValueDifferentYValuesThrowsError()
+		{
+			double[] x = {5, 5};
+			double[] y = {7, 19};
+			Assert.Throws (typeof(ArgumentException), () => pi.fit (x, y));
+		}
+		[Test]
+		public void TestSameXValueSameYValuesReturnsTheSameYForX()
+		{
+			double[] x = {5, 5};
+			double[] y = {7, 7};
+			pi.fit (x, y);
+			Assert.AreEqual(7, pi.predict(5));
+		}
+		[Test]
+		public void TestSameXValueSameYValuesThrowsErrorForOutOfBoundsX()
+		{
+			double[] x = {5, 5};
+			double[] y = {7, 7};
+			pi.fit (x, y);
+			Assert.Throws (typeof(ArgumentException), () => pi.predict(6));
+			Assert.Throws (typeof(ArgumentException), () => pi.predict(4));
 		}
 	}
 }
